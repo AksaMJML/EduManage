8d1c8c159833b086aa2fb4e1ade28faad4c2c36c
AksaMJML <akaks0186@gmail.com>
2026-01-22 10:54:18 +0530

    declared attributes to student class

---
+++ src/main/java/edu/icet/model/dto/StudentDTO.java
@@ -0,0 +1,20 @@
+package edu.icet.model.dto;
+
+import edu.icet.util.Gender;
+
+public class StudentDTO {
+
+    private String id;
+    private String name;
+    private int age;
+    private String address;
+    private int cityId; // Enum , table or api call
+    private Gender gender;
+    private int classId;
+    private int clubId;
+    private int libraryId;
+    private int schoolId;
+    private int achievementId;
+    private int mediumId;
+    private int reportId;
+}
