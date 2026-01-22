df4045e79ccba7dcfddc6b59844f17a183c1c5eb
AksaMJML <akaks0186@gmail.com>
2026-01-22 21:10:28 +0530

    add CRUD operations to StudentController for managing students

--- src/main/java/edu/icet/controller/StudentController.java
+++ src/main/java/edu/icet/controller/StudentController.java
@@ -4,22 +4,41 @@ import edu.icet.model.dto.StudentDTO;
 import edu.icet.service.StudentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-
+@RequestMapping("/students")
 public class StudentController {
 
     @Autowired
     StudentService studentService;
 
-    @PostMapping("/add-student")
+    @PostMapping("/add")
     public void addStudent(@RequestBody StudentDTO studentDTO){
         studentService.addStudent(studentDTO);
     }
 
+    @PutMapping("/update")
+    public void updateStudent(@RequestBody StudentDTO studentDTO){
+        studentService.updateStudent(studentDTO);
+    }
+
+    @DeleteMapping("/delete-by-id/{id}")
+    public void deleteByID(@PathVariable Integer id){
+        studentService.deleteStudent(id);
+    }
+
+    @GetMapping("/search-by-id/{id}")
+    public StudentDTO serachByID(@PathVariable Integer id){
+        return studentService.searchById(id);
+    }
+
+    @GetMapping("/get-all")
+    public List<StudentDTO> getAll(){
+        return studentService.getAllStudents();
+    }
 
 }
