6ae5d97ab90c2528e8fc22d72d1f160456f645bf
AksaMJML <akaks0186@gmail.com>
2026-01-22 15:44:44 +0530

    implement update, delete, search, and getAll methods in StudentServiceImpl

--- src/main/java/edu/icet/service/impl/StudentServiceImpl.java
+++ src/main/java/edu/icet/service/impl/StudentServiceImpl.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -26,21 +27,29 @@ public class StudentServiceImpl implements StudentService {
 
     @Override
     public void updateStudent(StudentDTO studentDTO) {
-
+        studentRepositery.save(mapper.map(studentDTO, StudentEntity.class));
     }
 
     @Override
     public void deleteStudent(Integer id) {
-
+        studentRepositery.deleteById(id);
     }
 
     @Override
     public StudentDTO searchById(Integer id) {
-        return null;
+        StudentEntity studentEntity = studentRepositery.findById(id).get();
+        StudentDTO studentDTO = mapper.map(studentEntity, StudentDTO.class);
+        return studentDTO;
     }
 
     @Override
     public List<StudentDTO> getAllStudents() {
-        return List.of();
+        List<StudentEntity> studentEntities = studentRepositery.findAll();
+        ArrayList<StudentDTO> studentDTOArrayList = new ArrayList<>();
+        studentEntities.forEach(studentEntity -> {
+            StudentDTO studentDTO = mapper.map(studentEntity, StudentDTO.class);
+            studentDTOArrayList.add(studentDTO);
+        });
+        return studentDTOArrayList;
     }
 }
