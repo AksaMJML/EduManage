59fa2cbaff893dd32244ed6d75fd12cdc0d1f28d
AksaMJML <akaks0186@gmail.com>
2026-01-22 22:23:49 +0530

    update SchoolEntity and SchoolServiceImpl to include ID generation and mapping logic

--- src/main/java/edu/icet/model/entity/SchoolEntity.java
+++ src/main/java/edu/icet/model/entity/SchoolEntity.java
@@ -1,6 +1,8 @@
 package edu.icet.model.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,6 +15,8 @@ import lombok.ToString;
 @Entity
 public class SchoolEntity {
 
+    @Id
+    //@GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
     private String id;
     private String name;
     private String code;
--- src/main/java/edu/icet/service/impl/SchoolServiceImpl.java
+++ src/main/java/edu/icet/service/impl/SchoolServiceImpl.java
@@ -1,17 +1,23 @@
 package edu.icet.service.impl;
 
 import edu.icet.model.dto.SchoolDTO;
+import edu.icet.model.entity.SchoolEntity;
 import edu.icet.service.SchoolService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 public class SchoolServiceImpl implements SchoolService {
 
-
+    final SchoolService service;
+    ModelMapper mapper;
 
     @Override
     public void registerSchool(SchoolDTO schoolDTO) {
-
+        SchoolDTO entity = mapper.map(schoolDTO, SchoolEntity.class);
+        service.registerSchool(entity);
     }
 
     @Override
