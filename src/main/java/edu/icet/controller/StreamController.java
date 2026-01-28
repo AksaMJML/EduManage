b63949b69e37061fed4e6bc21e3ff436595ef8b5
AksaMJML <akaks0186@gmail.com>
2026-01-29 00:10:19 +0530

    add StreamController methods for stream management and Swagger documentation

--- src/main/java/edu/icet/controller/StreamController.java
+++ src/main/java/edu/icet/controller/StreamController.java
@@ -2,12 +2,16 @@ package edu.icet.controller;
 
 import edu.icet.model.dto.StreamDTO;
 import edu.icet.service.StreamService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/streams")
 @RequiredArgsConstructor
+@Tag(name = "Stream Controller", description = "APIs for managing streams")
 public class StreamController {
 
     final StreamService service;
@@ -21,4 +25,20 @@ public class StreamController {
     public void updateStream(@RequestBody StreamDTO streamDTO){
         service.updateStream(streamDTO);
     }
+
+    @DeleteMapping("/delete-by-id/{id}")
+    public void deleteById(@PathVariable Integer id){
+        service.deleteStream(id);
+    }
+
+    @GetMapping("/search-by-id/{id}")
+    public StreamDTO searchById(@PathVariable Integer id){
+        return service.searchById(id);
+    }
+
+    @GetMapping("/get-all")
+    public List<StreamDTO> getAllStreams(){
+        return service.getAllStreams();
+    }
+
 }
